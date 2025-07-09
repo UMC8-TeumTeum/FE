7d90cc2c9da7005abf39dc5f65a7ed38995faf0d
Santoragi32 <woojj1254577@naver.com>
2025-07-09 16:04:42 +0900

    [FEAT/#9] 닉네임, 직종 입력 화면 작성

---
+++ app/src/main/java/com/example/teumteum/SignUp/OnBoardingNicknameFragment.kt
@@ -0,0 +1,32 @@
+package com.example.teumteum.SignUp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teumteum.R
+import com.example.teumteum.databinding.FragmentAgreementBinding
+import com.example.teumteum.databinding.FragmentOnBoardingNicknameBinding
+
+class OnBoardingNicknameFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardingNicknameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOnBoardingNicknameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? SignUpActivity)?.setProgressBar(25)
+    }
+}
