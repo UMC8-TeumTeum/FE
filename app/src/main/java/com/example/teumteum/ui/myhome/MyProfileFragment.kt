1e25e457c6e32115acd036206a6337f234fbd814
Santoragi32 <woojj1254577@naver.com>
2025-07-17 17:41:48 +0900

    [FEAT/#33] 마이페이지 -> 내 프로필 화면 이동

--- app/src/main/java/com/example/teumteum/ui/myhome/MyProfileFragment.kt
+++ app/src/main/java/com/example/teumteum/ui/myhome/MyProfileFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.teumteum.R
 import com.example.teumteum.databinding.FragmentMyProfileBinding
+import com.example.teumteum.ui.friend.Friend02SuggestFragment
+import com.example.teumteum.ui.friend.FriendRequestCardAdapter
+import com.example.teumteum.ui.main.MainActivity
 
 class MyProfileFragment : Fragment() {
 
@@ -25,4 +29,15 @@ class MyProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? MainActivity)?.hideBottomBar()
+
+        binding.backBtn.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, MyhomeFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
 }
--- app/src/main/java/com/example/teumteum/ui/myhome/MyhomeFragment.kt
+++ app/src/main/java/com/example/teumteum/ui/myhome/MyhomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.teumteum.ui.myhome
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.teumteum.R
 import com.example.teumteum.databinding.FragmentMyhomeBinding
+import com.example.teumteum.ui.main.MainActivity
+import com.example.teumteum.ui.signup.OnBoardingProfileFragment
+import com.example.teumteum.ui.signup.OnBoardingScheduleFragment
+import com.example.teumteum.ui.signup.SignUpActivity
 
 class MyhomeFragment : Fragment() {
 
@@ -19,5 +26,40 @@ class MyhomeFragment : Fragment() {
         binding = FragmentMyhomeBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileIv.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, MyProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.nicknameTv.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, MyProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.fieldTv.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, MyProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.arrowIv.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, MyProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as? MainActivity)?.showBottomBar()
     }
 }
