2e254387a40336125790fa8be07a418bcd44ff1c
벨라 <icomksh@gmail.com>
2025-07-18 00:43:11 +0900

    [#35] Feat: 빈틈채우기 위시 등록 중간커밋(등록버튼 위치 수정 필요)

--- app/src/main/java/com/example/teumteum/ui/wish/WishlistRVAdapter.kt
+++ app/src/main/java/com/example/teumteum/ui/wish/WishlistRVAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.teumteum.ui.wish
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.teumteum.R
 import com.example.teumteum.data.entities.WishItem
 import com.example.teumteum.databinding.ItemWishlistBinding
+import com.example.teumteum.ui.signup.OnBoardingProfileFragment
 
 class WishlistRVAdapter(private var wishlist: List<WishItem>, private val fragmentManager: FragmentManager) : RecyclerView.Adapter<WishlistRVAdapter.ViewHolder>() {
 
@@ -19,13 +22,27 @@ class WishlistRVAdapter(private var wishlist: List<WishItem>, private val fragme
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = wishlist[position]
         val binding = holder.binding
-        binding.tvWishTitle.text = item.title
+        binding.wishTitleTv.text = item.title
         binding.wishTimeTv.text = item.time
 
         binding.root.setOnClickListener {
             val bottomSheet = WishEditFragment.newInstanceWithWishDummy(item)
             bottomSheet.show(fragmentManager, bottomSheet.tag)
         }
+
+        binding.fillButton.setOnClickListener {
+            val fragment = WishSetting01Fragment().apply {
+                arguments = Bundle().apply {
+                    putString("title", binding.wishTitleTv.text.toString())
+                }
+            }
+
+            fragmentManager.beginTransaction()
+                .replace(R.id.main_frm, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
     }
 
     override fun getItemCount(): Int = wishlist.size
