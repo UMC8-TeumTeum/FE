70f85705cfa1809a2dbbd830fc9b8b9abbc7a8dd
Santoragi32 <woojj1254577@naver.com>
2025-07-17 16:03:24 +0900

    [FEAT/#33] 마이페이지 - 내 프로필

---
+++ app/src/main/java/com/example/teumteum/ui/myhome/MyProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.example.teumteum.ui.myhome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teumteum.R
+import com.example.teumteum.databinding.FragmentMyProfileBinding
+
+class MyProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMyProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
