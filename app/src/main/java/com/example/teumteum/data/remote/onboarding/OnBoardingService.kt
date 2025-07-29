22352e8177226731f1c051ef8e601dbead213e79
Santoragi32 <woojj1254577@naver.com>
2025-07-29 16:48:45 +0900

    [FEAT/#93] 닉네임 입력 API 연결

---
+++ app/src/main/java/com/example/teumteum/data/remote/onboarding/OnBoardingService.kt
@@ -0,0 +1,53 @@
+package com.example.teumteum.data.remote.onboarding
+
+import android.util.Log
+import com.example.teumteum.data.remote.onboarding.dto.NicknameJobRequest
+import com.example.teumteum.data.remote.onboarding.dto.NicknameJobResponse
+import com.example.teumteum.data.remote.wish.WishRetrofitInterface
+import com.example.teumteum.data.remote.wish.dto.RegisterWishRequest
+import com.example.teumteum.data.remote.wish.dto.RegisterWishResponse
+import com.example.teumteum.ui.signup.view.NicknameJobFieldView
+import com.example.teumteum.utils.getRetrofitWithToken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.jvm.java
+
+class OnBoardingService {
+
+    private lateinit var nicknameJobFieldView: NicknameJobFieldView
+
+    fun setNicknameJobFieldView(nicknameJobFieldView: NicknameJobFieldView) {
+        this.nicknameJobFieldView = nicknameJobFieldView
+    }
+
+    //온보딩 : 닉네임, 직종 입력
+    fun postNicknameAndJobField(request: NicknameJobRequest) {
+        val nicknameJobApi = getRetrofitWithToken().create(OnBoardingRetrofitInterface::class.java)
+        val call = nicknameJobApi.postNicknameAndJobField(request)
+
+        call.enqueue(object : Callback<NicknameJobResponse> {
+            override fun onResponse(
+                call: Call<NicknameJobResponse>,
+                response: Response<NicknameJobResponse>
+            ) {
+                if(response.isSuccessful) {
+                    val body = response.body()
+                    if(body != null && body.code == "ONBOARDING2002") {
+                        nicknameJobFieldView.onNicknameJobSuccess(body.code)
+                    } else {
+                        nicknameJobFieldView.onNicknameJobFailure(body?.code ?: "UNKNOWN", body?.message)
+                    }
+                } else {
+                    nicknameJobFieldView.onNicknameJobFailure("HTTP_${response.code()}", response.errorBody()?.string())
+                }
+            }
+
+            override fun onFailure(call: Call<NicknameJobResponse>, t: Throwable) {
+                nicknameJobFieldView.onNicknameJobFailure("NETWORK_ERROR", t.localizedMessage)
+            }
+
+        })
+
+    }
+}
