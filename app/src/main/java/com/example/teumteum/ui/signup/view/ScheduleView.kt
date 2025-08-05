84439d90e10b572c306276a11f5615fe0fc47a97
Santoragi32 <woojj1254577@naver.com>
2025-08-05 14:34:30 +0900

    [REFACTOR/#133] signup.view 제거

--- app/src/main/java/com/example/teumteum/ui/signup/view/ScheduleView.kt
+++
@@ -1,6 +0,0 @@
-package com.example.teumteum.ui.signup.view
-
-interface ScheduleView {
-    fun onScheduleSuccess(code: String)
-    fun onScheduleFailure(code: String, message: String?)
-}
--- app/src/main/java/com/example/teumteum/ui/signup/view/SleepPatternView.kt
+++
@@ -1,6 +0,0 @@
-package com.example.teumteum.ui.signup.view
-
-interface SleepPatternView {
-    fun onSleepPatternSuccess(code: String)
-    fun onSleepPatternFailure(code: String, message: String?)
-}
