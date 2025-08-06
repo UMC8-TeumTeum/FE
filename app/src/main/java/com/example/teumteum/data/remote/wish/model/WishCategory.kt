59fe0b628123e06da7894a72ef096ab03f3e53e7
벨라 <icomksh@gmail.com>
2025-08-06 12:54:23 +0900

    [FEAT/#84] 온보딩 리마인드 알림 조회 카테고리 정보 조회 api 연동(테스트 필요)

--- app/src/main/java/com/example/teumteum/data/remote/wish/model/WishCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teumteum.data.remote.wish.model
-
-import com.google.gson.annotations.SerializedName
-
-data class WishCategory(
-    @SerializedName("id") val categoryId: Long,
-    @SerializedName("name") val categoryName: String
-)
