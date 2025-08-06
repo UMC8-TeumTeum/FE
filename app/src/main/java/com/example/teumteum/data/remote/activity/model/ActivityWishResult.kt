ca9dd993d769ba1f12939df5f397a19865b75c66
벨라 <icomksh@gmail.com>
2025-08-07 08:50:08 +0900

    [FEAT/#118] 위시 dto content -> title 수정

--- app/src/main/java/com/example/teumteum/data/remote/activity/model/ActivityWishResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.teumteum.data.remote.activity.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ActivityWishResult(
-    @SerializedName("id") val id: Long,
-    @SerializedName("content") val content: String,
-    @SerializedName("estimatedDuration") val estimatedDuration: String
-)
