2a0f4f373482a876771ab7654bf30b4cc934736c
벨라 <icomksh@gmail.com>
2025-08-15 22:40:41 +0900

    [REFACTOR/#196] 채움 활동 결과 화면에서 친구 화면으로 이동하는 코드 추가

--- app/src/main/java/com/example/teumteum/ui/activity/FillingActivity02Fragment.kt
+++ app/src/main/java/com/example/teumteum/ui/activity/FillingActivity02Fragment.kt
@@ -17,6 +17,7 @@ import com.example.teumteum.databinding.FragmentFillingActivity02Binding
 import com.example.teumteum.ui.activity.adapter.AiRecommendRVAdapter
 import com.example.teumteum.ui.activity.adapter.WishRecommendRVAdapter
 import com.example.teumteum.ui.activity.viewModel.ActivityViewModel
+import com.example.teumteum.ui.friend.FriendFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -67,6 +68,13 @@ class FillingActivity02Fragment : Fragment() {
             parentFragmentManager.setFragmentResult("reset_form", Bundle.EMPTY)
 
             requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.fillingActivityFriendSearchCv.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, FriendFragment())
+                .addToBackStack(null)
+                .commit()
         }
 
         // 새로고침: 시머 -> 재조회
