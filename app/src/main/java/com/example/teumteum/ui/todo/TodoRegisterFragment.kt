02a9cc64478cd2f8f0728e1bd0cda4bf8feaaba8
벨라 <icomksh@gmail.com>
2025-08-22 22:25:37 +0900

    [BUG/#259] 키보드 올라왔을 때 하단 버튼 올라오지 않게 수정

--- app/src/main/java/com/example/teumteum/ui/todo/TodoRegisterFragment.kt
+++ app/src/main/java/com/example/teumteum/ui/todo/TodoRegisterFragment.kt
@@ -18,6 +18,8 @@ import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -187,6 +189,29 @@ class TodoRegisterFragment : BottomSheetDialogFragment(), IDateClickListener{
                 // 겹침/플리커 방지를 위해 즉시 커밋
                 tx.commitNowAllowingStateLoss()
             }
+        }
+
+        // 원래 스크롤뷰 패딩 저장
+        val originalBottomPadding = binding.registerScroll.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+
+            // 버튼 실제 높이
+            val btnH = binding.btnTodoRegister.height
+
+            // 스크롤 영역: 키보드 + 버튼 높이만큼 바닥 패딩
+            binding.registerScroll.setPadding(
+                binding.registerScroll.paddingLeft,
+                binding.registerScroll.paddingTop,
+                binding.registerScroll.paddingRight,
+                if (imeVisible) originalBottomPadding + btnH else originalBottomPadding
+            )
+
+            // 키보드 올라왔을 때 보이는 흰색 영역 제거
+            binding.btnTodoRegister.visibility = if (imeVisible) View.GONE else View.VISIBLE
+
+            insets
         }
 
         binding.startDateTv.setOnClickListener {
--- app/src/main/java/com/example/teumteum/ui/wish/WishRegisterFragment.kt
+++ app/src/main/java/com/example/teumteum/ui/wish/WishRegisterFragment.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -83,6 +85,29 @@ class WishRegisterFragment : BottomSheetDialogFragment() {
                 // 겹침/플리커 방지를 위해 즉시 커밋
                 tx.commitNowAllowingStateLoss()
             }
+        }
+
+        // 원래 스크롤뷰 패딩 저장
+        val originalBottomPadding = binding.registerScroll.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+
+            // 버튼 실제 높이
+            val btnH = binding.btnWishRegister.height
+
+            // 1) 스크롤 영역: 키보드 + 버튼 높이만큼 바닥 패딩
+            binding.registerScroll.setPadding(
+                binding.registerScroll.paddingLeft,
+                binding.registerScroll.paddingTop,
+                binding.registerScroll.paddingRight,
+                if (imeVisible) originalBottomPadding + btnH else originalBottomPadding
+            )
+
+            // 키보드 올라왔을 때 보이는 흰색 영역 제거
+            binding.btnWishRegister.visibility = if (imeVisible) View.GONE else View.VISIBLE
+
+            insets
         }
 
         binding.btnWishRegister.setOnClickListener {
