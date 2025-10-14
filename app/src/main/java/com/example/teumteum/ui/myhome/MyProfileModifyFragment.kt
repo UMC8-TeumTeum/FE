300dfae6ef795585653fe3102e51c2934867f0d9
Santoragi <woojj1254577@naver.com>
2025-10-14 13:29:06 +0900

    [REFACTOR/#276] 프로필 수정 화면 뷰모델 연결

--- app/src/main/java/com/example/teumteum/ui/myhome/MyProfileModifyFragment.kt
+++ app/src/main/java/com/example/teumteum/ui/myhome/MyProfileModifyFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.example.teumteum.R
 import com.example.teumteum.databinding.FragmentMyProfileBinding
 import com.example.teumteum.databinding.FragmentMyProfileModifyBinding
 import com.example.teumteum.ui.main.MainActivity
+import com.example.teumteum.ui.main.viewModel.HomeViewModel
+import com.example.teumteum.ui.myhome.viewModel.MyHomeViewModel
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class MyProfileModifyFragment : Fragment() {
 
     private lateinit var binding: FragmentMyProfileModifyBinding
+    private val viewModel: MyHomeViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,37 @@ class MyProfileModifyFragment : Fragment() {
         binding.cancelTv.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
+
+        viewModel.nickname.observe(viewLifecycleOwner) { nickname ->
+            binding.profileNicknameTv.text = nickname ?: "닉네임"
+        }
+
+        viewModel.field.observe(viewLifecycleOwner) { field ->
+            binding.profileFieldTv.text = if (!field.isNullOrBlank()) "$field" else "직업 없음"
+        }
+
+        homeViewModel.teumTimeDays.observe(viewLifecycleOwner) { updateTeumTime() }
+        homeViewModel.teumTimeHours.observe(viewLifecycleOwner) { updateTeumTime() }
+        homeViewModel.teumTimeMinutes.observe(viewLifecycleOwner) { updateTeumTime() }
+
+        viewModel.profileImageUrl.observe(viewLifecycleOwner) { imageUrl ->
+            if (!imageUrl.isNullOrBlank()) {
+                Glide.with(this)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.gray_teum) // 기본 이미지 리소스
+                    .error(R.drawable.gray_teum)       // 에러 시 이미지
+                    .into(binding.profileIv)
+            } else {
+                binding.profileIv.setImageResource(R.drawable.gray_teum)
+            }
+        }
+    }
+
+    private fun updateTeumTime() {
+        val days = homeViewModel.teumTimeDays.value ?: 0
+        val hours = homeViewModel.teumTimeHours.value ?: 0
+        val minutes = homeViewModel.teumTimeMinutes.value ?: 0
+        binding.profileTimerTv.text = "${days}일 ${hours}시간 ${minutes}분"
     }
 
 }
