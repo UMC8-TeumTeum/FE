be5b568213d32c73c941f22483e0ea3c575bbb91
Santoragi <woojj1254577@naver.com>
2025-10-17 15:57:32 +0900

    [FEAT/#280] 리마인드 알림 설정 화면 추가

---
+++ app/src/main/java/com/example/teumteum/ui/myhome/MyRemindAlarmSettingFragment.kt
@@ -0,0 +1,24 @@
+package com.example.teumteum.ui.myhome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teumteum.R
+
+class MyRemindAlarmSettingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_my_remind_alarm_setting, container, false)
+    }
+
+}
