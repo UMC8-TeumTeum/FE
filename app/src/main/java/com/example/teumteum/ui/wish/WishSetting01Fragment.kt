576c8e7a5e47566c5bb45d76d6eb7ea948c0ba84
벨라 <icomksh@gmail.com>
2025-12-30 18:26:16 +0900

    [BUG/#329] observer 누락 수정

--- app/src/main/java/com/example/teumteum/ui/wish/WishSetting01Fragment.kt
+++ app/src/main/java/com/example/teumteum/ui/wish/WishSetting01Fragment.kt
@@ -205,6 +205,14 @@ class WishSetting01Fragment : Fragment() {
         }
 
         updateIndicator(isAM)
+
+        homeViewModel.sleepTimeList.observe(viewLifecycleOwner) {
+            clockAdapter.refreshAll()
+        }
+
+        homeViewModel.todoTimeList.observe(viewLifecycleOwner) {
+            clockAdapter.refreshAll()
+        }
     }
 
     private fun setupObservers() {
