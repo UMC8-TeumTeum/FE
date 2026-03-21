5801c3a1463ce485947e5463331b6a1b0ccc768f
벨라 <icomksh@gmail.com>
2026-03-21 16:24:34 +0900

    [REFACTOR/#479] 로그 출력 및 주석 수정

--- app/src/main/java/com/umc/teumteum/ui/friend/Friend02RejectBottomSheetFragment.kt
+++ app/src/main/java/com/umc/teumteum/ui/friend/Friend02RejectBottomSheetFragment.kt
@@ -11,7 +11,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.umc.teumteum.R
 import com.umc.teumteum.databinding.BottomSheetFriend02RejectBinding
@@ -56,7 +55,7 @@ class Friend02RejectBottomSheetFragment : BottomSheetDialogFragment() {
 
         val item = viewModel.selectedTeum.value
         if (item == null) {
-            dismiss() // 선택값 없으면 닫기
+            dismiss()
             return
         }
 
@@ -78,11 +77,10 @@ class Friend02RejectBottomSheetFragment : BottomSheetDialogFragment() {
             val responseId = arguments?.getInt("responseId") ?: return@setOnClickListener
 
             if (selectedOption == SelectedOption.SUGGEST) {
-                // Friend02RequestFragment에서 받아온 teumList 전달 필요
+                // Friend02RequestFragment에서 받아온 teumList 전달
                 val parentFragment = parentFragmentManager.fragments.firstOrNull { it is Friend02RequestFragment } as? Friend02RequestFragment
                 val teumList = parentFragment?.teumList ?: emptyList()
 
-                // 시간 제안 화면 이동
                 parentFragmentManager.beginTransaction()
                     .replace(
                         R.id.main_frm,
@@ -95,7 +93,7 @@ class Friend02RejectBottomSheetFragment : BottomSheetDialogFragment() {
                 val status = "rejected"
 
                 Log.d("REJECT_BOTTOM_SHEET", "responseId: $responseId, status: $status")
-                Toast.makeText(requireContext(), "삭제되었습니다", Toast.LENGTH_SHORT).show()
+                Log.d("REJECT_BOTTOM_SHEET", "틈 요청을 거절하였습니다.")
 
                 // 응답 처리
                 viewModel.respondToTeum(responseId, status)
